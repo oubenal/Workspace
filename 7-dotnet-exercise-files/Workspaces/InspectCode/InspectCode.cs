e260134d757266dbcf6b23d3185e0788a4cdd5f2
Redouane Oubenal <oubenal@gmail.com>
2019-10-31 23:06:25 +0100

    using CommandLineUtils

--- 7-dotnet-exercise-files/Workspaces/InspectCode/InspectCode.cs
+++ 7-dotnet-exercise-files/Workspaces/InspectCode/InspectCode.cs
@@ -1,45 +1,36 @@
 ﻿using System;
 using System.Diagnostics;
+using Microsoft.Extensions.CommandLineUtils;
 
 namespace RunProcess
 {
     public class InspectCode
     {
+        #region private
         private const string REPORT_OUTPUT_FOLDER = @"C:\Users\ouben\AppData\InspectCode";
-        public static string ReportPath(string slnPath)
+
+        private const string OUTPUT = "--output |-o";
+        private const string PROJECT = "--project";
+        private const string PROFILE = "--profile";
+        private const string ABSOLUTE_PATH = "--absolute-paths |-a";
+        private const string HELP_FLAG = "-? |-h |--help";
+
+        private static string ReportPath(string slnPath)
         {
             var slnName = System.IO.Path.GetFileNameWithoutExtension(slnPath);
             return $@"{REPORT_OUTPUT_FOLDER}\{slnName}.report.xml";
-        } 
-        static string GetInspectCodeArgument(string slnPath)
-        {
-            var args = new[] {
-                @"--project=*",
-                $@"--output={ReportPath(slnPath)}",
-                @"--profile=C:\Users\ouben\AppData\Roaming\JetBrains\Shared\vAny\GlobalSettingsStorage.DotSettings", //Default path for computer settings
-                @"--absolute-paths",
-                @"--swea",
-                @"--severity=Warning",
-                @"--verbosity=INFO",
-                @"--no-buildin-settings",
-                @"--cache-home=%LOCALAPPDATA%\InspectCode\cache\solution_name",
-                @"--properties:Platform=x86;Configuration=Release",
-                $"{slnPath}",
-        };
-            return string.Join(" ", args);
         }
-        
-        public static string Run(string slnPath)
+        #endregion
+        public static string Run(string slnPath, string arguments)
         {
             try
             {
-                string arguments = GetInspectCodeArgument(slnPath);
                 var process = new Process
                 {
                     StartInfo = new ProcessStartInfo
                     {
                         FileName = @"C:\Users\ouben\ThirdParties\inspectcode.exe",
-                        Arguments = arguments,
+                        Arguments = $"{arguments} {slnPath}",
                         UseShellExecute = false,
                         RedirectStandardOutput = true,
                         CreateNoWindow = true
@@ -58,10 +49,48 @@ namespace RunProcess
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.Message);
+                Console.WriteLine(e.GetBaseException().Message);
             }
 
             return ReportPath(slnPath);
+        }
+
+        public static int RunInspections(string[] args)
+        {
+            var app = new CommandLineApplication(true);
+
+            app.HelpOption(HELP_FLAG);
+
+            var slnPathArgument = app.Argument("solution path", "solution to analyse");
+
+            var outputOption = app.Option(OUTPUT, "output result path", CommandOptionType.SingleValue);
+            var ProjectOption = app.Option(PROJECT, "project to analyse as a Regex", CommandOptionType.SingleValue);
+            var ProfileOption = app.Option(PROFILE, "dotsetting file path", CommandOptionType.SingleValue);
+            var AbsolutePathsOption = app.Option(ABSOLUTE_PATH, "use absolute path in report", CommandOptionType.NoValue);
+
+            string argument = "";
+            app.OnExecute(() =>
+            {
+                if (outputOption.HasValue())
+                    argument += $"--output={outputOption.Value()} ";
+                if (ProjectOption.HasValue())
+                    argument += $"--project={ProjectOption.Value()} ";
+                if (ProfileOption.HasValue())
+                    argument += $"--profile={ProfileOption.Value()} ";
+                if (AbsolutePathsOption.HasValue())
+                    argument += $"--absolute-paths ";
+
+                var slnPath = slnPathArgument.Value;
+                if (!string.IsNullOrWhiteSpace(slnPath))
+                    Run(slnPath, argument);
+                else
+                    app.ShowHelp();
+                return 0;
+            });
+
+            app.Description = "InspectCode wrapper";
+
+            return app.Execute(args);
         }
     }
 }
--- 7-dotnet-exercise-files/Workspaces/InspectCode/Program.cs
+++ 7-dotnet-exercise-files/Workspaces/InspectCode/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace RunProcess
+﻿using System;
+
+namespace RunProcess
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static int Main(string[] args)
         {
-            InspectCode.Run(@"C:\Users\ouben\source\repos\TeamCitySharp\TeamCitySharp.sln");
+#if DEBUG
+            var slnPath = @"C:\RandD\Roslyn-Example\7-dotnet-exercise-files\Workspaces\Workspaces.sln";
+            args = new[] {
+                @"--project=*",
+                $@"--output={ReportPath(slnPath)}",
+                @"--profile=C:\Users\ouben\AppData\Roaming\JetBrains\Shared\vAny\GlobalSettingsStorage.DotSettings", //Default path for computer settings
+                @"--absolute-paths",
+                @"--swea",
+                @"--severity=Warning",
+                @"--verbosity=INFO",
+                @"--no-buildin-settings",
+                @"--cache-home=%LOCALAPPDATA%\InspectCode\cache\solution_name",
+                @"--properties:Platform=x86;Configuration=Release",
+                $"{slnPath}",
+            };
+#endif
+            try
+            {
+                return InspectCode.RunInspections(args);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.GetBaseException().Message);
+                return 0xbad;
+            }
         }
+        private const string REPORT_OUTPUT_FOLDER = @"C:\Users\ouben\AppData\InspectCode";
+        private static string ReportPath(string slnPath)
+        {
+            var slnName = System.IO.Path.GetFileNameWithoutExtension(slnPath);
+            return $@"{REPORT_OUTPUT_FOLDER}\{slnName}.report.xml";
+        }
+
     }
 }
